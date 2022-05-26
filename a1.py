15167be6749f35d8e50a566366fef16845b8e047
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-05-26 14:46:00 -0500

    Print_Input Asks for user input, and prints it!

---
+++ a1.py
@@ -0,0 +1,17 @@
+'''
+A very simple script!
+'''
+
+def myfunc():
+    '''
+    A simple Function
+    '''
+
+    first = 'Your input was: '
+    second = input('Enter something to be printed: ')
+
+    print(first)
+    print(second)
+
+
+myfunc()
